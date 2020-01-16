dcdddddef096a6654cd873cc45b0a8020f830920
Mikkel V <56712674+MVardinghus@users.noreply.github.com>
2020-01-16 08:50:29 +0100

    Add files via upload

--- aws_functions.py
+++ aws_functions.py
@@ -10,12 +10,17 @@ Created on Thu Dec 19 11:35:16 2019
     Mikkel Vardinghus.
 """
 
-#import spidev
-from time import time, ctime, sleep
+import spidev
+from time import time, sleep, ctime
 import requests
 import smtplib
 from twilio.rest import Client #Twilio skal installeres (pip install twilio)
+import pytz
+from datetime import datetime
+from subprocess import Popen, PIPE
 
+# Filstil til fejlskrivning
+filePath        = '/home/pi/Documents/project1/Vandspil/errorlog.txt'
 
 ###############################################################################
 # Funktion til initialisering af ADC'en.
@@ -40,7 +45,6 @@ def readADC(ADC, channel, vref):
     Andet indeks specificerer hvilken kanal.
     Tredje indeks er ligegyldig data, som dog stadig skal sendes.
     """
-    
     reply       =    ADC.xfer2([0b00000001,channel, 0b00000000])
     roomValue   =    ((reply[1]&3) << 8) + reply[2]
     roomVolts   =    (roomValue*vref)/1024
@@ -51,103 +55,160 @@ def readADC(ADC, channel, vref):
 ###############################################################################
 # Thingspeak overførselsfunktion
 
-def thingSpeakTransfer(channelID, writeKey, temp1, temp2):
+def thingspeakTransfer(channelID, writeKey, temp1, temp2):
     """
     Denne funktion tager som input Thingspeak kanal ID, Thingspeak write key,
     og 2 temperaturer. Dataen bliver herefter gemt i en dictionary, 
     og sendes herefter til Thingspeak som json-data.
     """
-    #time = find ud af hvordan den finder ud af det, tid?
+
     try:
         url = f"https://api.thingspeak.com/channels/{channelID}/bulk_update.json"
         
-        timestamp = datetime.now(tz = pytz.timezone('CET')).isoformat()
+        timeStamp = datetime.now(tz = pytz.timezone('CET')).isoformat()
         data = {
                 "write_api_key" : writeKey,
                 "updates"       : [{
-                        "created_at"    : timestamp,
+                        "created_at"    : timeStamp,
                         "field1"        : temp1,
                         "field2"        : temp2
                         }]}
         
-        response = requests.post(url,json=data)
-        status = response.status_code
+        requests.post(url,json=data)
+        #response = requests.post(url,json=data)
+        #status = response.status_code
         
-    except:
-        print("ThingSpeak transfer failed.")
+    except Exception as errorMsg:
+        errorLog(filePath, errorMsg)
+        pass
+        
     return
 
 ###############################################################################
 # Trigger-funktion
 
-def checkTempState(roomTemp, pipeTemp):
+def checkTempState(roomTemp, pipeTemp, counter, timeTrigger):
     """
-    Denne funktion modtager som input rumtemperatur og rørtemperatur,
-    sammenligner de to, og returnerer en booleansk værdi afhængigt af
-    differencen. Hvis der er en difference på mere end 1 grad over en
-    periode på 1 døgn, returnerer funktionen True, hvis ikke, False.
+    Modtager som input rumtemperatur, rørtemperatur, counter og et tidsstempel.
+    
+    De to temperaturer sammenlignes, og såfremt der er forskel på mere end 1
+    grad, samtidig med at counteren er 0, skabes et nyt tidsstempel,
+    og der bliver lagt 1 til counteren.
+    
+    Normaliserer temperaturforskellen sig nulstiller tidsstempel og counter.
+    
+    Tidsstemplet bliver herefter sammenlignet med nuværende tid,
+    og såfremt differencen vedvarer i et døgn returnerer funktionen True på
+    trigger variablen.
     """
-    tempDiff = roomTemp-pipeTemp
     trigger = False
-    timeTrigger = 90000000*5
-    if tempDiff < -1 or tempDiff > 1:
-        timeTrigger = time.time() + 5
-#        timeTrigger = time.time() + 60*1440 Original
-    if time.time() >= timeTrigger:
+    tempDiff = roomTemp-pipeTemp
+    
+    if (tempDiff < -1 or tempDiff > 1):
+        tempTrigger = True
+    else:
+        tempTrigger = False
+        
+    if tempTrigger and counter == 0:
+        timeTrigger = time() + 60*1440
+        counter += 1
+    
+    if not tempTrigger:
+        counter = 0
+        timeTrigger = 10**100
+            
+    if time() >= timeTrigger:
+        print("Der er sendt besked")
         trigger = True
+        counter = 0
 
-    return trigger
+    return trigger, counter, timeTrigger
 
 ###############################################################################
 # E-mail alarm funktion
     
-def AlarmEmail(smtpHost, smtpPort, sMail, sPass, rMail):
+def emailAlert(smtpHost, smtpPort, sMail, sPass, rMail):
     """
-    Hvad glor du på?
+    Funktion til at give besked om vandspild over email.
     """
-    # Laver en SMTP session
-    s = smtplib.SMTP(smtpHost, smtpPort)
-     
-    # Starter tls for sikkerhed.
-    s.starttls()
-     
-    #  afsenders credidentials
-    s.login(sMail, sPass)
-     
-    # Variabel med besked.
-    textmessage = "Du har vandspild."
-    message = 'Subject: {}\n\n{}'.format('VANDSPILD!', textmessage)
-    
-     
-    # send emailen
-    s.sendmail(sMail, rMail, message)
-     
-    # lukker smtp sessionen igen.
-    s.quit()
+    try:
+        # Laver en SMTP session
+        s = smtplib.SMTP(smtpHost, smtpPort)
+         
+        # Starter tls for sikkerhed.
+        s.starttls()
+         
+        #  afsenders credidentials
+        s.login(sMail, sPass)
+         
+        # Variabel med besked.
+        textmessage = "Du har vandspild."
+        message = 'Subject: {}\n\n{}'.format('VANDSPILD!', textmessage)
+        
+        # send emailen
+        s.sendmail(sMail, rMail, message)
+         
+        # lukker smtp sessionen igen.
+        s.quit()
+    except Exception as errorMsg:
+        errorLog(filePath, errorMsg)
+        pass
     return
 
 ###############################################################################
 # SMS alarm funktion (Husk at lav ny demo bruger)
 
-def SMSMsg(accountSID,authToken,messageBody,sNum,rNum):
+def smsAlert(accountSID,authToken,messageBody,sNum,rNum):
     """
-    Kommentar tekst
+    Funktion til at give besked om vandspild over sms.
     """
-    client = Client(accountSID, authToken)
-    
-    #Danner beskeden
-    message = client.messages \
-                    .create(
-                         body=messageBody,
-                         from_=sNum,
-                         to=rNum
-                     )
+    try:
+        client = Client(accountSID, authToken)
+        
+        #Danner beskeden
+        message = client.messages \
+                        .create(
+                             body=messageBody,
+                             from_=sNum,
+                             to=rNum
+                         )
+        print(message)
+    except Exception as errorMsg:
+        errorLog(filePath, errorMsg)
+        pass
     return
 
 ###############################################################################
-# Test funktion
-    
-def timeTest():
-    f = datetime.now()
-    print(f)
+# Funktion til at genstarte Raspberry Pi'en. Brugt i tilfælde af fejl.
+
+def restart():
+    """
+    Funktion til at genstarte Raspberry Pi'en. Bliver anvendt i tilfælde af
+    at programmet crasher.
+    """
+    sleep(60)
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    process = Popen(command.split(), stdout=PIPE)
+    output = process.communicate()[0]
+    print(output)
+    return
+
+###############################################################################
+# Funktion til at logge fejlkoder.
+
+def errorLog(filePath, errorMsg):
+    """
+    Kommentar
+    """
+    try:
+        timeStamp = ctime()
+        logMsg = timeStamp + ":" + "\t" + str(errorMsg) + "\n"
+        with open(filePath, mode="a") as f:
+            f.write(str(logMsg))
+    except FileNotFoundError:
+        pass
+    except PermissionError:
+        pass
+    finally:
+        f.close()
     return
--- aws_main.py
+++ aws_main.py
@@ -13,10 +13,8 @@ Created on Thu Dec 19 11:35:16 2019
 ##############################################################################
 """
 To do:
-    - E-mail og SMS funktioner skal kun kunne køre 1 gang i døgnet
-    - Kommentarer
-    - Exception handling
-    - Github synkronisering
+    - Ændrer crontab fra aws_source.py til aws_main.py
+    - Exceptions
 """
 ##############################################################################
 
@@ -49,24 +47,29 @@ smsMessage      = 'Der er registreret vandspild på din enhed.'
 sNum            = '+19726395292'
 rNum            = '+4593886999' # Nummeret skal være verified på Twilio.
 
+# Variabler til fejl-logning
+filePath        = '/home/pi/Documents/project1/Vandspil/errorlog.txt'
+
+
 # PROGRAM KODE STARTER HER
-
-running = True
-MCP3008 = aws.initADC(1)
-
 try:
+    
+    running = True
+    MCP3008 = aws.initADC(1)
+
     while running:
         
         roomTemp  = round(aws.readADC(MCP3008,roomTempChannel,vref),1)
         pipeTemp  = round(aws.readADC(MCP3008,pipeTempChannel,vref),1)
         print(f'Rum temp: {roomTemp} | Rør temp: {pipeTemp}')
             
-        aws.thingSpeakTransfer(channelID, writeKey, roomTemp, pipeTemp)
+        aws.thingspeakTransfer(channelID, writeKey, roomTemp, pipeTemp)
     
         trigger,counter,timeTrigger  = aws.checkTempState(roomTemp, pipeTemp, counter, timeTrigger)
         
         if trigger:
-            aws.AlarmEmail(smtpHost, smtpPort, sMail, sPass, rMail)
-            aws.SMSMsg(accountSID, authToken, smsMessage, sNum, rNum)
-except:
+            aws.emailAlert(smtpHost, smtpPort, sMail, sPass, rMail)
+            aws.smsAlert(accountSID, authToken, smsMessage, sNum, rNum)
+except Exception as errorMsg:
+    aws.errorLog(filePath, errorMsg)
     aws.restart()
